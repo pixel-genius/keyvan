034d13faecf0f1d1e371e7a6750a05869ca022d7
Pouria Kazemirad <yp.kazemirad@yarhis.com>
2025-08-02 17:52:35 +0330

    pwa config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import nextPwa from "next-pwa";
 
 const nextConfig: NextConfig = {
   output: "standalone",
@@ -6,10 +7,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'https://tanbaku.com',
-        port: '3000',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "https://tanbaku.com",
+        port: "3000",
+        pathname: "/**",
       },
     ],
     unoptimized: true, // This can help with some image loading issues
@@ -32,4 +33,23 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withPWA = nextPwa({
+  dest: "public", // where service-worker.js will be generated
+  disable: process.env.NODE_ENV === "development", // disable in dev
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "offlineCache",
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+});
+
+export default withPWA(nextConfig);
--- src/app/_components/CardItem.tsx
+++ src/app/_components/CardItem.tsx
@@ -12,25 +12,32 @@ const CardItem = ({
   link?: string;
 }) => {
   return (
-    
-      <Link href={link || ""}>
-    <div className="flex flex-col items-center">
-      <div className="bg-card p-4 rounded-xl shadow-lg  flex items-center justify-center">
-        {typeof icon === "string" ? (
-          <Image src={icon} alt={label} width={24} height={24} unoptimized />
-        ) : (
-          icon
-        )}
+    <Link href={link || ""}>
+      <div className="flex flex-col items-center">
+        <div className="bg-card p-4 rounded-xl shadow-lg  flex items-center justify-center">
+          {typeof icon === "string" ? (
+            <Image
+              src={icon}
+              alt={label}
+              width={24}
+              height={24}
+              unoptimized
+              style={{ width: "auto", height: "auto" }}
+              className="object-contain"
+            />
+          ) : (
+            icon
+          )}
+        </div>
+        <Typography
+          variant="label/xs"
+          weight="normal"
+          className="text-center pt-4"
+        >
+          {label}
+        </Typography>
       </div>
-      <Typography
-        variant="label/xs"
-        weight="normal"
-        className="text-center pt-4"
-      >
-        {label}
-      </Typography>
-      </div>
-      </Link>
+    </Link>
   );
 };
 
